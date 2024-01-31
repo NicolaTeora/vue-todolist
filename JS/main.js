74da8e8c88936ca9a208f642c7ca23106573e25f
NicolaTeora <nikxodia@gmail.com>
2024-01-31 21:25:17 +0100

    feat : add metodh for mileston 3

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
+            titleApp: 'To Do list Vue',
             //// oggetto con proprietà text e done
             list:[
 
@@ -32,13 +33,28 @@ const app = createApp({
                 },
             ],
 
-            listToDo: [],
-
-            titleApp: 'To Do list Vue',
+            newToDo: {
+                text:'',
+                done: false,
+            },
         };
     },
 
-    methods: {},
+    methods: {
+        /*
+        * cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
+        * che quindi viene aggiunto alla lista dei todo esistenti.
+        */
+        addNewTodo() {
+            const copyNewTodo = {...this.newToDo}; // !
+            // TODO: if (copyNewTodo.length == 0) 
+            console.log(this.newToDo) 
+            console.log(copyNewTodo)
+
+            this.list.push(this.copyNewTodo); // ! provare a passare la copia non reattiva non funziona
+            this.newToDo.text= ''
+        }
+    },
 
     mounted() {},
 });
